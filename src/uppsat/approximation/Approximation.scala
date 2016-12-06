ebecf7f85669aebddf72f4dddc51d87190197838
Aleksandar Zeljic <aleksandar.zeljic@it.uu.se>
2016-12-06 13:21:33 +0100

    Start of template approximation

--- src/uppsat/approximation/Approximation.scala
+++ src/uppsat/approximation/Approximation.scala
@@ -4,25 +4,44 @@ import uppsat.ModelReconstructor.Model
 import uppsat.precision.PrecisionMap
 import uppsat.precision.PrecisionOrdering
 import uppsat.ast.AST
+import uppsat.ast.ConcreteSort
 import uppsat.theory.Theory
+import uppsat.precision.PrecisionOrdering
+import uppsat.ast.FunctionSymbol
+import uppsat.precision.PrecisionMap.Path
 
-trait Approximation[T] {
+trait Approximation[P] {
   
   val inputTheory : Theory
   val outputTheory : Theory
-  val precisionOrdering : PrecisionOrdering[T]
-  def satRefine(ast : AST, decodedModel : Model, failedModel : Model, pmap : PrecisionMap[T]) : PrecisionMap[T]  
-  def unsatRefine(ast : AST, core : List[AST], pmap : PrecisionMap[T]) : PrecisionMap[T]
-  def encodeFormula(ast : AST, pmap : PrecisionMap[T]) : AST
-  def decodeModel(ast : AST, appModel : Model, pmap : PrecisionMap[T]) : Model
+  val precisionOrdering : PrecisionOrdering[P]
+  def satRefine(ast : AST, decodedModel : Model, failedModel : Model, pmap : PrecisionMap[P]) : PrecisionMap[P]  
+  def unsatRefine(ast : AST, core : List[AST], pmap : PrecisionMap[P]) : PrecisionMap[P]
+  def encodeFormula(ast : AST, pmap : PrecisionMap[P]) : AST
+  def decodeModel(ast : AST, appModel : Model, pmap : PrecisionMap[P]) : Model
   def reconstruct(ast : AST, decodedModel : Model) : Model
   
  }
 
-trait TemplateApproximation[T] extends Approximation[T] {
-  
-  //up/down
-  //castingFunction (sort, precision, precision)
+
+
+trait TemplateApproximation[P] extends Approximation[P] {  
+  def encodeNode(symbol : FunctionSymbol, children : List[AST], precision : P) : AST
+  def cast(ast : AST, source : ConcreteSort, target : ConcreteSort  ) : AST
   //errorFunction
   //nodeByNodeTranslationFunctions
+  
+  def encodeAux(ast : AST, path : Path, pmap : PrecisionMap[P]) : AST = {
+    val AST(symbol, label, children) = ast
+    val newChildren = 
+      for ((c, i) <- children zip children.indices) yield {
+        encodeAux( c, i :: path, pmap)
+      }    
+    val AST(functionSymbol, _, encChildren) = encodeNode(symbol, newChildren, pmap(path))
+    AST(functionSymbol, path, encChildren)
+  }
+  
+  def encodeFormula(ast : AST, pmap : PrecisionMap[P]) : AST = {
+    encodeAux(ast, List(0), pmap)
+  }
 }
