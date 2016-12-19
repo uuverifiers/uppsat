bf724c53e9ccecc35abfe3097b02446e38c2005f
Peter Backeman <peter@backeman.se>
2016-12-19 13:20:07 +0100

    Changed back to Philipps parser

--- src/uppsat/parser/Environment.scala
+++ src/uppsat/parser/Environment.scala
@@ -8,22 +8,34 @@ import scala.collection.mutable.Map
 
 class Environment {
   var symbols : Map[String, ConcreteFunctionSymbol] = Map()
+  var definitions : Map[String, AST] = Map()
   var assumptions : List[AST] = List()
 
   def addSymbol(id : String, symbol : ConcreteFunctionSymbol) = {
     symbols += id -> symbol
+  }
+  
+  def addDefinition(id : String, definition : AST) = {
+    definitions += id -> definition
   }
 
   def addAssumption(ass : AST) = {
     assumptions = ass :: assumptions
   }
   
-  def find(id : String) : Option[ConcreteFunctionSymbol] = {
+  def findSymbol(id : String) : Option[ConcreteFunctionSymbol] = {
     if (symbols contains id)
       Some(symbols(id))
     else
       None 
   }
+  
+  def findDefinition(id : String) : Option[AST] = {
+    if (definitions contains id)
+      Some(definitions(id))
+    else
+      None 
+  }  
 
   def lookup(id : String) = {
     symbols.find(_._1 == id).get._2
