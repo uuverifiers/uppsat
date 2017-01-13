82a838810d2b9a06dead35664e3b2acd507d9838
Aleksandar Zeljic <aleksandar.zeljic@it.uu.se>
2017-01-13 17:22:21 +0100

    Rewriting model

--- src/main/scala/uppsat/ModelReconstructor.scala
+++ src/main/scala/uppsat/ModelReconstructor.scala
@@ -9,9 +9,69 @@ import ast.AST
 import uppsat.solver.SMTSolver
 import uppsat.solver.SMTTranslator
 import uppsat.solver.Z3OnlineSolver
+import uppsat.ast.ConcreteFunctionSymbol
+import uppsat.theory.FloatingPointTheory.FPVar
 
 object ModelReconstructor {
-  type Model = Map[Path, AST]
+  //type Model = Map[Path, AST]
+  
+  class Model() {
+    var variableValuation : Map[ConcreteFunctionSymbol, AST] = Map()
+    var subexprValuation : Map[Path, AST] = Map()
+    
+    
+    def set(ast : AST, value : AST) = {
+      if (contains(ast)){
+        throw new Exception("Reassigning  a model value")
+      }
+      
+      ast match {
+        case AST(symbol, path, children) => {
+          symbol match {
+            case _ if (symbol.theory.isVariable(symbol)) => 
+              variableValuation = variableValuation + (symbol -> value)
+            case _ if (children.length == 0) =>
+              ()
+            case _ => 
+              subexprValuation = subexprValuation + (path -> value)
+          }
+        }
+        case _ => throw new Exception("Requesting a non-AST model value!")
+      }
+    }
+    
+    def contains(ast : AST) : Boolean = {
+      ast match {
+        case AST(symbol, path, children) => {
+          symbol match {
+            case _ if (symbol.theory.isVariable(symbol)) => 
+              variableValuation.contains(symbol)
+            case _ if (children.length == 0) =>
+              true
+            case _ => 
+              subexprValuation.contains(path)
+          }
+        }
+        case _ => throw new Exception("Requesting a non-AST model value!")
+      }
+    }
+    
+    def apply(ast : AST) : AST = {
+      ast match {
+        case AST(symbol, path, children) => {
+          symbol match {
+            case _ if (symbol.theory.isVariable(symbol)) => 
+              variableValuation(symbol)
+            case _ if (children.length == 0) =>
+              ast
+            case _ => 
+              subexprValuation(path)
+          }
+        }
+        case _ => throw new Exception("Requesting a non-AST model value!")
+      }
+    }
+  }
   
   var onlineSolver = None : Option[SMTSolver]
   
