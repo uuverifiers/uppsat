605359f4f5a5298efc098339922ce34b4cc14ede
Unknown <peter@backeman.se>
2017-11-10 16:27:52 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/scala/uppsat/parser/Interpreter.scala
+++ src/main/scala/uppsat/parser/Interpreter.scala
@@ -753,6 +753,7 @@ object Interpreter {
       
       def toBinary(n:Int, bin: List[Int] = List.empty[Int]) : List[Int] = {
         if(n/2 == 1) (1:: (n % 2) :: bin)
+        if(n == 0) List(0)
         else {
           val r = n % 2
           val q = n / 2
