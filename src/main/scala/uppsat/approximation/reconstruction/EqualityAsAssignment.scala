79a3b0b511f979e5412961a8258dad7cecf76874
Unknown <peter@backeman.se>
2017-11-27 09:49:46 +0100

    Refcator equality as assignmetn

--- src/main/scala/uppsat/approximation/reconstruction/EqualityAsAssignment.scala
+++ src/main/scala/uppsat/approximation/reconstruction/EqualityAsAssignment.scala
@@ -1,7 +1,5 @@
 package uppsat.approximation.reconstruction
 
-
-
 import uppsat.globalOptions._
 import uppsat.approximation.ModelReconstruction
 import scala.collection.mutable.{ListBuffer, Set}
@@ -29,118 +27,42 @@ import uppsat.approximation.toolbox.Toolbox
 import scala.collection.mutable.Queue
 import scala.collection.mutable.Stack
 import uppsat.theory.FloatingPointTheory.FloatingPointPredicateSymbol
+import uppsat.globalOptions
 
 
 trait EqualityAsAssignmentReconstruction extends ModelReconstruction {
   
-  def trySet(lhs : AST, rhs : AST, candidateModel : Model) : Boolean = {
-    val lhsDefined = candidateModel.contains(lhs)
-    val rhsDefined = candidateModel.contains(rhs)
-  
-    val ret = 
-      (lhs.isVariable, rhs.isVariable) match {
-        case (true, true) => {
-          (lhsDefined, rhsDefined) match {
-            case (false, true) => {
-              verbose(">> " + lhs.symbol + " " + lhs.label + " " + " <- " + candidateModel(rhs).symbol + "/" + rhs.symbol + " " + rhs.label + "/")  
-              candidateModel.set(lhs, candidateModel(rhs))
-              true
-            }
-            case (true, false) => {
-              verbose(">> " + rhs.symbol + " " + rhs.label + " " + " <- " + candidateModel(lhs).symbol + "/" + lhs.symbol + " " + lhs.label + "/")
-              candidateModel.set(rhs, candidateModel(lhs))
-              true
-            }
-            case (false, false) => {
-              false
-            }
-            case (true, true) => {
-              false
-            }
-          }
-        }
-        case (true, false) if (!lhsDefined) => {
-          verbose(">> " + lhs.symbol + " " + lhs.label + " " + " <- " + candidateModel(rhs).symbol + "/" + rhs.symbol + " " + rhs.label + "/")
-          candidateModel.set(lhs, candidateModel(rhs))
-          true
-        }
-        case (false, true) if (!rhsDefined) =>{
-          verbose(">> " + rhs.symbol + " " + rhs.label + " " + " <- " + candidateModel(lhs).symbol + "/" + lhs.symbol + " " + lhs.label + "/")
-          candidateModel.set(rhs, candidateModel(lhs))
-          true
-        }
-        case (_, _) => {
-          false
-        }
-    }
-    ret
-    
-  }
-  
-  def equalityAsAssignment2(ast : AST, decodedModel : Model,  candidateModel : Model) : Boolean = {
-    val ret = 
-      if (ast.symbol.sort == BooleanSort && decodedModel(ast).symbol == BoolTrue) { 
-        ast.symbol match {
-          case RoundingModeEquality => trySet(ast.children(0), ast.children(1), candidateModel)
-          case fpPred : FPPredicateSymbol => trySet(ast.children(0), ast.children(1), candidateModel)
-          case idxSym : IndexedFunctionSymbol => 
-            if (idxSym.getFactory == FPEqualityFactory || idxSym.getFactory == FPFPEqualityFactory)
-              trySet(ast.children(0), ast.children(1), candidateModel)
-            else
-              false
-//          case BoolEquality => trySet(ast.children(0), ast.children(1), candidateModel)
-          case other => false
-        } 
-      } else {
-        false
-      }
-    if (ret) {
-      verbose("> " + ast.symbol + " " + ast.label + " " + " <- " + BoolTrue)
-      candidateModel.set(ast, BoolTrue)
-    }
-    ret
-  }
-
   
  def equalityAsAssignment(ast : AST, decodedModel : Model,  candidateModel : Model) : Boolean = {
     ast match { 
-      //        case AST(BoolEquality, _, _) | 
-      //             AST(IntEquality, _, _)| 
-      case AST(RoundingModeEquality, _ , _)| 
-          AST(_: FPPredicateSymbol, _, _) 
-          if (decodedModel(ast).symbol == BoolTrue && 
+      case AST(RoundingModeEquality, _ , _)|
+           AST(BoolEquality, _, _) | 
+           AST(IntEquality, _, _)|       
+           AST(_: FPPredicateSymbol, _, _) 
+           if (decodedModel(ast).symbol == BoolTrue && 
                 (! ast.symbol.isInstanceOf[IndexedFunctionSymbol] 
                    || ast.symbol.asInstanceOf[FPPredicateSymbol].getFactory == FPEqualityFactory 
                    || ast.symbol.asInstanceOf[FPPredicateSymbol].getFactory == FPFPEqualityFactory)) => { 
-            val lhs = ast.children(0) 
-            val rhs = ast.children(1) 
-            val lhsDefined = candidateModel.contains(lhs) 
-            val rhsDefined = candidateModel.contains(rhs) 
- 
-            (lhs.isVariable, rhs.isVariable) match { 
-              case (true, true) => { 
-                (lhsDefined, rhsDefined) match { 
-                  case (false, true) => {
-                    candidateModel.set(lhs, candidateModel(rhs)) 
-                    true 
-                  }
-                  case (true, false) => {
-                    candidateModel.set(rhs, candidateModel(lhs)) 
-                    true 
-                  }
-                  case (false, false) | (true, true) => {
-                    false 
-                  }
-                } 
-              } 
-              case (true, false) if (!lhsDefined) => {                 
+            val (lhs, rhs) = (ast.children(0), ast.children(1))
+            val (lhsDefined, rhsDefined) = (candidateModel.contains(lhs), candidateModel.contains(rhs))
+
+            ((lhs.isVariable, lhsDefined), (rhs.isVariable, rhsDefined)) match {
+              case ((true, false), (true, true)) => {
                 candidateModel.set(lhs, candidateModel(rhs)) 
-                true 
-              } 
-              case (false, true) if (!rhsDefined) =>{                                     
+                true                
+              }
+              case ((true, true), (true, false)) => {
                 candidateModel.set(rhs, candidateModel(lhs)) 
-                true 
-              } 
+                true                
+              }              
+              case ((true, false), (false, _)) => {
+                candidateModel.set(lhs, candidateModel(rhs)) 
+                true                 
+              }
+              case ((false, _), (true, false)) => {
+                candidateModel.set(rhs, candidateModel(lhs)) 
+                true                 
+              }              
               case (_, _) => {
                 false 
               }
@@ -158,14 +80,15 @@ trait EqualityAsAssignmentReconstruction extends ModelReconstruction {
       val newChildren = for ( c <- children) yield {
         Toolbox.getCurrentValue(c, decodedModel, candidateModel)
       }
-
       
       val newAST = AST(symbol, label, newChildren.toList)
       val newValue = ModelEvaluator.evalAST(newAST, inputTheory)
       verbose(ast.symbol + " " + ast.label + " " + " <- " + newValue.symbol)
       
       candidateModel.set(ast, newValue)      
-      ast.ppWithModels("", decodedModel, candidateModel, false)
+      
+      if(globalOptions.VERBOSE)
+        ast.ppWithModels("", decodedModel, candidateModel, false)
     }
     candidateModel
   }
@@ -210,62 +133,10 @@ trait EqualityAsAssignmentReconstruction extends ModelReconstruction {
         
       }
     }
-    
-//    var eqDependency = new HashMap[AST, Set[ConcreteFunctionSymbol]] with MultiMap[AST, ConcreteFunctionSymbol]
-//    
-//
-//    for ( eq <- toEvaluateEquality.toList) {
-//      val lhsVars = eq.children(0).iterator.filter(_.isVariable)
-//      val rhsVars = eq.children(1).iterator.filter(_.isVariable)
-//      
-//      if (eq.children(0).isVariable) {
-//        for (v <- rhsVars)
-//          eqDependency.addBinding(eq, v.symbol)
-//      }
-//
-//      if (eq.children(1).isVariable) {
-//        for (v <- lhsVars)
-//          eqDependency.addBinding(eq, v.symbol)
-//      }
-//      
-//      if (!eq.children(0).isVariable && !eq.children(1).isVariable) {
-//        val variables = eq.children.map(_.iterator.filter(_.isVariable)).flatten
-//        for (v <- variables)
-//          eqDependency.addBinding(eq, v.symbol)      
-//      }
-//    }   
-//          
-//    for ( eq <- toEvaluateEquality.toList) {
-//      val variables = eq.children.map(_.iterator.filter(_.isVariable)).flatten
-//      for (v <- variables) {
-//        eqDependency.addBinding(eq, v.symbol)
-//      }
-//    }
-    
-    type Implication = (scala.collection.immutable.Set[ConcreteFunctionSymbol], ConcreteFunctionSymbol)
-    
-    import scala.collection.mutable.{ListBuffer}
-    import scala.collection.immutable.Set
-
       
     val equalitySort = Toolbox.topologicalSortEqualities(toEvaluateEquality.toList)
-    //val equalitySort = toEvaluateEquality.toList 
-      
-    val eqSet1 = toEvaluateEquality.toSet
-    val eqSet2 = equalitySort.toSet
-    
-      
-      
-//    println("non-sorted equalities:")
-//    for (eq <- eqSet1) 
-//      eq.prettyPrint("$")
-//    println("topological sorted equalities: ")
-//    for (eq <- eqSet2)
-//      eq.prettyPrint("£")
-//      println("end")
-    
+        
     equalitySort.map(reconstructSubtree(_, decodedModel, candidateModel))
-    //toEvaluateEquality.map(reconstructSubtree(_, decodedModel, candidateModel))
     toReconstructPredicate.map(reconstructSubtree(_, decodedModel, candidateModel))
     toEvaluateBoolean.map(reconstructNode(decodedModel, candidateModel, _))
     candidateModel
