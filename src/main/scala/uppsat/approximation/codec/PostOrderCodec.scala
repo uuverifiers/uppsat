518590d174b1360e1aa01e9ca06cf4f5056a8e11
Peter Backeman <peter@backeman.se>
2021-04-03 23:49:33 +0200

    Refactor Codec

--- src/main/scala/uppsat/approximation/codec/PostOrderCodec.scala
+++ src/main/scala/uppsat/approximation/codec/PostOrderCodec.scala
@@ -1,17 +1,30 @@
 package uppsat.approximation.codec
 
-import uppsat.ast.AST
-import uppsat.ast.AST.Label
-
-import uppsat.precision.PrecisionOrdering
-import uppsat.precision.PrecisionMap
 import uppsat.ModelEvaluator.Model
 import uppsat.approximation._
+import uppsat.ast.AST
+import uppsat.ast.AST.Label
 import uppsat.ast.ConcreteFunctionSymbol
+import uppsat.precision.{PrecisionMap, PrecisionOrdering}
 
+/** The PostOrderCodec allows for describing codec which moves over a formula in
+  * a post-order fashion.
+  *
+  */
 trait PostOrderCodec extends Codec {
-  def encodeNode(symbol : ConcreteFunctionSymbol, label : Label, children : List[AST], precision : Precision) : AST 
-  def decodeNode(args : (Model, PrecisionMap[Precision]), decodedModel : Model, ast : AST) : Model
+  /** Encode a single node.
+    *
+    */
+  def encodeNode(symbol : ConcreteFunctionSymbol,
+                 label : Label,
+                 children : List[AST],
+                 precision : Precision) : AST
+
+  /** Decode a single model value.
+    *
+    */
+  def decodeNode(args : (Model, PrecisionMap[Precision]),
+                 decodedModel : Model, ast : AST) : Model
   
   def encodeFormula(ast : AST, pmap : PrecisionMap[Precision]) : AST = { 
     val AST(symbol, label, children) = ast
@@ -22,7 +35,9 @@ trait PostOrderCodec extends Codec {
     encodeNode(symbol, ast.label, newChildren, pmap(ast.label))
   }
 
-  def decodeModel(ast : AST, appModel : Model, pmap : PrecisionMap[Precision]) : Model = {
+  def decodeModel(ast : AST,
+                  appModel : Model,
+                  pmap : PrecisionMap[Precision]) : Model = {
     val decodedModel = new Model()
     AST.postVisit(ast, decodedModel, (appModel, pmap), decodeNode)
     decodedModel
