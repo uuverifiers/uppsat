79a1280f3508dcd00324b0f104eba88e813fc4f4
Peter Backeman <peter@backeman.se>
2021-04-04 08:28:29 +0200

    Refactor refinement
    Refactor PostOrderRefinement.scala, UniformRefinement.scala, ErrorBasedRefinement.scala

--- src/main/scala/uppsat/approximation/refinement/ErrorBasedRefinement.scala
+++ src/main/scala/uppsat/approximation/refinement/ErrorBasedRefinement.scala
@@ -1,22 +1,26 @@
 package uppsat.approximation.refinement
 
-import uppsat.ast.AST
 import uppsat.ModelEvaluator
 import uppsat.ModelEvaluator.Model
+import uppsat.ast.AST
 import uppsat.approximation._
-import uppsat.precision._
 import uppsat.approximation.toolbox.Toolbox
 import uppsat.globalOptions._
+import uppsat.precision._
 
-// Error based refinement strategy uses a measure of error to
-// determine which precisions need to be refined
+
 // TODO: (Aleks) "Magic" numbers, I don't understand them
+/** Error based refinement strategy uses a measure of error to determine which
+  * precisions need to be refined */
 trait ErrorBasedRefinementStrategy extends PostOrderRefinement {
   val fractionToRefine : Double
   val precisionIncrement : Precision
 
-  def nodeError(decodedModel : Model)(failedModel : Model)(accu : Map[AST, Double], ast : AST) : Map[AST, Double]
-  
+  /** Computes a (double) error value for a node. */
+  def nodeError(decodedModel : Model)
+               (failedModel : Model)
+               (accu : Map[AST, Double], ast : AST) : Map[AST, Double]
+
   def defaultRefinePrecision( p : Precision) : Precision = {
      precisionOrdering.+(p, precisionIncrement)
   }
@@ -27,22 +31,33 @@ trait ErrorBasedRefinementStrategy extends PostOrderRefinement {
     d1.compareTo(d2) > 0
   }
 
-  override def satRefine(ast : AST, decodedModel : Model, failedModel : Model, pmap : PrecisionMap[Precision])
+  override def satRefine(ast : AST,
+                         decodedModel : Model,
+                         failedModel : Model,
+                         pmap : PrecisionMap[Precision])
       : PrecisionMap[Precision] = {
     val accu = Map[AST, Double]()
-    val errorRatios = AST.postVisit(ast, accu, nodeError(decodedModel)(failedModel))
-    val sortedErrRatios = errorRatios.toList.sortWith((x, y) => cmpErrors(x._2, y._2))
-    val k = math.ceil(fractionToRefine * sortedErrRatios.length).toInt //TODO: Assertions
+    val errorRatios =
+      AST.postVisit(ast, accu, nodeError(decodedModel)(failedModel))
+    val sortedErrRatios =
+      errorRatios.toList.sortWith((x, y) => cmpErrors(x._2, y._2))
 
-    val relevant = Toolbox.booleanComparisonOfModels(ast, decodedModel, failedModel)
-    val toRefine = sortedErrRatios.filter( x => relevant.contains(x._1)).map(_._1)
+    // TODO: Assertions
+    val k =
+      math.ceil(fractionToRefine * sortedErrRatios.length).toInt
+
+    val relevant =
+      Toolbox.booleanComparisonOfModels(ast, decodedModel, failedModel)
+    val toRefine =
+      sortedErrRatios.filter( x => relevant.contains(x._1)).map(_._1)
 
     var newPMap = pmap
     var changes = 0
     for (node <-
-         toRefine.filter(
-           x => precisionOrdering.lt(newPMap(x.label),  pmap.pOrd.maximalPrecision)
-         ).take(k)) {
+         toRefine.filter{
+           x =>
+           precisionOrdering.lt(newPMap(x.label), pmap.pOrd.maximalPrecision)
+         }.take(k)) {
       newPMap = newPMap.update(node.label, satRefinePrecision(node, newPMap))
       changes += 1
     }
--- src/main/scala/uppsat/approximation/refinement/UniformRefinement.scala
+++ src/main/scala/uppsat/approximation/refinement/UniformRefinement.scala
@@ -1,32 +1,36 @@
 package uppsat.approximation.refinement
 
+import uppsat.ModelEvaluator.Model
 import uppsat.approximation._
 import uppsat.ast.AST
 import uppsat.precision._
-import uppsat.ModelEvaluator.Model
 
+/** Uniform strategy - apply same operation to each node. */
 trait UniformPGRefinementStrategy extends ProofGuidedRefinementStrategy {
   def unsatRefinePrecision(p : Precision) : Precision
-  
-  def unsatRefine(ast : AST, core : List[AST], pmap : PrecisionMap[Precision]) : PrecisionMap[Precision] = {
+  def unsatRefine(ast : AST,
+                  core : List[AST],
+                  pmap : PrecisionMap[Precision]) : PrecisionMap[Precision] = {
     pmap.map(unsatRefinePrecision)
   }
 }
 
 
+/** Uniform strategy - apply same operation to each node. */
 trait UniformMGRefinementStrategy extends ModelGuidedRefinementStrategy {
   def satRefinePrecision(p : Precision) : Precision
-
-  def satRefine(ast : AST, decodedModel : Model, failedModel : Model, pmap : PrecisionMap[Precision])  = {
+  def satRefine(ast : AST,
+                decodedModel : Model,
+                failedModel : Model,
+                pmap : PrecisionMap[Precision])  = {
     pmap.map(satRefinePrecision)
-  }  
+  }
 }
 
+/** Uniform strategy - apply same operation to each node. */
 trait UniformRefinementStrategy extends UniformMGRefinementStrategy
                                    with UniformPGRefinementStrategy {
   def increasePrecision(p : Precision) : Precision
-  
   def satRefinePrecision(p : Precision) = increasePrecision(p)
-  
   def unsatRefinePrecision(p : Precision) = increasePrecision(p)
 }
