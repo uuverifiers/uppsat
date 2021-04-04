799249015285d822eeb6ee391a8cff1993201e72
Peter Backeman <peter@backeman.se>
2021-04-04 20:32:53 +0200

    Refactor test
    regression.scala

--- src/test/scala/regression.scala
+++ src/test/scala/regression.scala
@@ -9,20 +9,19 @@ import uppsat.globalOptions
 import uppsat.globalOptions._
 
 class Regression extends FunSpec {
-  
+
   def getListOfFiles(dir: File, extensions: List[String]): List[File] = {
     dir.listFiles.filter(_.isFile).toList.filter { file =>
         extensions.exists(file.getName.endsWith(_))
     }
   }
-   
+
    globalOptions.DEADLINE = Some(20000l)
    globalOptions.PARANOID = true
-   
+
    val satSources = new File(getClass.getResource("/sat/").toURI())
    val satFiles = getListOfFiles(satSources, List(".smt2"))
-   
-   
+
    describe("SAT : " ) {
      for (f <- satFiles) {
          println("Testing " + f)
@@ -30,19 +29,21 @@ class Regression extends FunSpec {
          val result = uppsat.main.main_aux(args)
          result match {
            case _ : Sat => 
-             it(f.toPath().toString().split('\\').reverse.head) {assert(true)}
+             it(f.toPath().toString().split('\\').reverse.head)
+             {assert(true)}
            case Unsat =>
-             it(f.toPath().toString().split('\\').reverse.head) {assert(false)}
+             it(f.toPath().toString().split('\\').reverse.head)
+             {assert(false)}
            case Unknown => 
-             ignore(f.toPath().toString().split('\\').reverse.head) {assert(false)}
-         }         
+             ignore(f.toPath().toString().split('\\').reverse.head)
+             {assert(false)}
+         }
        }
      }
-   
-   
+
    val unsatSources = new File(getClass.getResource("/unsat/").toURI())
    val unsatFiles = getListOfFiles(unsatSources, List(".smt2"))
-   
+
     describe("UNSAT : " ) {
      for (f <- unsatFiles) {
          println("Testing " + f)
@@ -50,11 +51,14 @@ class Regression extends FunSpec {
          val result = uppsat.main.main_aux(args)
          result match {
            case _ : Sat => 
-             it(f.toPath().toString().split('\\').reverse.head) {assert(false)}
+             it(f.toPath().toString().split('\\').reverse.head)
+             {assert(false)}
            case Unsat =>
-             it(f.toPath().toString().split('\\').reverse.head) {assert(true)}
+             it(f.toPath().toString().split('\\').reverse.head)
+             {assert(true)}
            case Unknown => 
-             ignore(f.toPath().toString().split('\\').reverse.head) {assert(false)}
+             ignore(f.toPath().toString().split('\\').reverse.head)
+             {assert(false)}
          }
      }
    }
